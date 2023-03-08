5ec25eeb4856377926a00bbae6d15fa7bef1d44a
Murad501 <mm845635@gmail.com>
2023-03-08 17:05:25 +0600

    remove product api created

--- index.js
+++ index.js
@@ -25,18 +25,74 @@ const run = async () => {
   try {
     const productCollections = client.db("baby-shop").collection("products");
     const categoryCollections = client.db("baby-shop").collection("categories");
+    const userCollections = client.db("baby-shop").collection("users");
+
+    // user
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const addedUser = await userCollections.findOne(query);
+      if (addedUser) {
+        return res.send({ acknowledged: true });
+      }
+      const result = await userCollections.insertOne(user);
+      res.send(result);
+    });
+
+    //sellers
+    app.get("/sellers", async (req, res) => {
+      const query = { role: "seller" };
+      const result = await userCollections.find(query).toArray();
+      res.send(result);
+    });
 
     // product
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const result = await productCollections.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/product", async (req, res) => {
       const product = req.body;
       const result = await productCollections.insertOne(product);
       res.send(result);
     });
 
+
+    //remove-product
+    app.delete("/remove-product/:id", async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollections.deleteOne(query)
+      res.send(result)
+    })
+
     // category
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const result = await categoryCollections.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/category", async (req, res) => {
       const category = req.body;
       const result = await categoryCollections.insertOne(category);
+      res.send(result);
+    });
+
+    app.get("/add-items", async (req, res) => {
+      const query = {};
+      const updateDoc = {
+        $set: {
+          category: {
+            _id: "640599e3c79cdd8a955871f8",
+            name: "Baby Gear",
+            picture: "https://i.ibb.co/n8phWgJ/gear.png",
+          },
+        },
+      };
+      const result = await productCollections.updateMany(query, updateDoc);
       res.send(result);
     });
   } catch {}
